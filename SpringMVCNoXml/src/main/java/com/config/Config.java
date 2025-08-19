50b9997727a7b04fa25c208db7db6789299ff4ee
Anjali Ganasala <160149065+anjaliganasala@users.noreply.github.com>
2025-08-19 13:56:53 +0530

    Add files via upload

---
+++ SpringMVCNoXml/src/main/java/com/config/Config.java
@@ -0,0 +1,22 @@
+package com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com")
+public class Config {
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		
+		InternalResourceViewResolver irvr = new InternalResourceViewResolver();
+		irvr.setPrefix("/WEB-INF/views/");
+		irvr.setSuffix(".jsp");
+		return irvr;
+				
+	}
+
+}
